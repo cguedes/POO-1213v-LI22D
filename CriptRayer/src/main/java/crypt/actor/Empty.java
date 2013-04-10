d92bb7499c9a0f4efa4a264655add6bd7f256333
Carlos Guedes <cguedes@gmail.com>
2013-04-10 12:09:13 +0100

    BUGFIX

--- CriptRayer/src/main/java/crypt/actor/Empty.java
+++ CriptRayer/src/main/java/crypt/actor/Empty.java
@@ -4,17 +4,8 @@ import crypt.Game;
 
 public class Empty extends Actor {
 
-  private static int GLOBAL_ID = 0;
-  private int id;
-
   public Empty(Point position, Game game) {
     super(' ', position, game);
-    id = GLOBAL_ID++ % 10;
-  }
-
-  @Override
-  public void draw() {
-    System.out.print(id);
   }
 
 }
