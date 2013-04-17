3413284c193d9786a978e9c9946d90b354520611
Carlos Guedes <cguedes@gmail.com>
2013-04-17 11:40:58 +0100

    Termina jogo quando se fecha a janela de direcções

--- CriptRayer/src/main/java/crypt/input/NonBlockingInput.java
+++ CriptRayer/src/main/java/crypt/input/NonBlockingInput.java
@@ -1,35 +1,39 @@
 package crypt.input;
+
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import java.awt.event.KeyListener;
-import java.awt.event.KeyEvent;
 
-public class NonBlockingInput extends AbstractInput 
-                              implements KeyListener {
-    
-    public NonBlockingInput() {
-      releaseAllKeys();
-      JFrame frame = new JFrame("NonBlockingInput");
-      frame.add(new JLabel("Choose direction (ASDW)"));
-      frame.addKeyListener(this);
-      frame.pack();
-      frame.setVisible(true);
+public class NonBlockingInput extends AbstractInput
+    implements KeyListener {
+
+  public NonBlockingInput() {
+    releaseAllKeys();
+    JFrame frame = new JFrame("NonBlockingInput");
+    frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    frame.add(new JLabel("Choose direction (ASDW)"));
+    frame.addKeyListener(this);
+    frame.pack();
+    frame.setVisible(true);
+  }
+
+  public void keyPressed(KeyEvent e) {
+    char key = Character.toUpperCase(e.getKeyChar());
+    if (Character.isLetter(key)) {
+      keysDown[key] = true;
     }
+  }
 
-   public void keyPressed(KeyEvent e)  {
-      char key = Character.toUpperCase(e.getKeyChar());
-      if(Character.isLetter(key)) {
-        keysDown[key] = true;
-      }
-   }
+  public void keyReleased(KeyEvent e) {
+    char key = Character.toUpperCase(e.getKeyChar());
+    if (Character.isLetter(key)) {
+      keysDown[key] = false;
+    }
+  }
 
-   public void keyReleased(KeyEvent e)  {
-      char key = Character.toUpperCase(e.getKeyChar());
-      if(Character.isLetter(key)) {
-        keysDown[key] = false;    
-      }
-   }
-   public void keyTyped(KeyEvent e) {}
-
+  public void keyTyped(KeyEvent e) {
+  }
 
 }
