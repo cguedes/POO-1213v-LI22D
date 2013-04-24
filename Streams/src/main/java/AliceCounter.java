c898d7abe0e77ed37d52c4a5f72d899edce1a92f
Carlos Guedes <cguedes@gmail.com>
2013-04-24 12:21:49 +0100

    Resolução do exercício proposto na aula.

--- Streams/src/main/java/AliceCounter.java
+++ Streams/src/main/java/AliceCounter.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
 import java.text.MessageFormat;
 
 public class AliceCounter {
@@ -13,12 +17,60 @@ public class AliceCounter {
   }
 
   private static int getNumberOfNonEmptyLines(String fileName) {
-    // TODO
-    return 0;
+    Reader reader = null;
+    int numEmptyLines = 0;
+    try {
+      // reader = new StringReader("Olá\n\nMundo.");
+      reader = new FileReader(fileName);
+      BufferedReader bufferedReader = new BufferedReader(reader);
+      String line = null;
+      while ((line = bufferedReader.readLine()) != null) {
+        line = line.trim();
+        if (!line.isEmpty())
+          ++numEmptyLines;
+      }
+      return numEmptyLines;
+    } catch (IOException ex) {
+      System.err.println("Error: " + ex.getMessage());
+      return -1;
+    } finally {
+      closeStream(reader);
+    }
   }
 
   private static int getTotalOccurencesOf(String textToFind, String fileName) {
-    // TODO
-    return 0;
+    Reader reader = null;
+    int numOccurs = 0;
+    try {
+      reader = new FileReader(fileName);
+      BufferedReader bufferedReader = new BufferedReader(reader);
+      String line = null;
+      while ((line = bufferedReader.readLine()) != null) {
+        numOccurs += getTotalOccurencesInLine(textToFind, line);
+      }
+      return numOccurs;
+    } catch (IOException ex) {
+      System.err.println("Error: " + ex.getMessage());
+      return -1;
+    } finally {
+      closeStream(reader);
+    }
+  }
+
+  private static int getTotalOccurencesInLine(String textToFind, String line) {
+    int idx = -1, count = 0;
+    while ((idx = line.indexOf(textToFind, idx + 1)) != -1) {
+      ++count;
+    }
+    return count;
+  }
+
+  private static void closeStream(Reader reader) {
+    try {
+      if (reader != null)
+        reader.close();
+    } catch (IOException ex) {
+      System.err.println("Cannot close the stream! " + ex.getMessage());
+    }
   }
 }
