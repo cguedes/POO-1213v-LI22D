6c2eab3536c7a7d4277c80a2f5ce975032ac75f4
Carlos Guedes <cguedes@gmail.com>
2013-05-29 11:15:33 +0100

    Adiciona implementação de quase todos os métodos de  ArrayList

--- Iterators/src/main/java/collections/ArrayList.java
+++ Iterators/src/main/java/collections/ArrayList.java
@@ -1,5 +1,10 @@
 package collections;
 
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
 public class ArrayList /* implements List<Integer> */{
 
   private Integer[] data;
@@ -13,8 +18,117 @@ public class ArrayList /* implements List<Integer> */{
     data = new Integer[initialCapacity];
   }
 
+  public boolean addAll(Collection<Integer> col) {
+    // ensureCapacity(col.size());
+    for (Integer val : col)
+      add(val);
+
+    // Iterable<Integer> iterable = (Iterable<Integer>) col;
+    // Iterator<Integer> iter = iterable.iterator();
+    // while (iter.hasNext()) {
+    // Integer val = iter.next();
+    // add(val);
+    // }
+
+    return true;
+  }
+
   public int size() {
     return size;
   }
 
+  public boolean add(Integer elem) {
+    // TODO: Ensure capacity
+    data[size++] = elem;
+    return true;
+  }
+
+  public Integer get(int idx) {
+    return data[idx];
+  }
+
+  public int indexOf(Object obj) {
+
+    if (obj == null)
+    {
+      for (int i = 0; i < size(); i++) {
+        if (get(i) == null)
+          return i;
+      }
+    }
+    else {
+      for (int i = 0; i < size(); i++) {
+        if (obj.equals(get(i)))
+          return i;
+      }
+    }
+
+    return -1;
+  }
+
+  public boolean contains(Object obj) {
+    return indexOf(obj) != -1;
+  }
+
+  public boolean isEmpty() {
+    return size() == 0;
+  }
+
+  public void clear() {
+    for (int i = 0; i < size; i++) {
+      data[i] = null;
+    }
+    size = 0;
+
+  }
+
+  public Integer[] toArray() {
+    return Arrays.copyOf(data, size);
+  }
+
+  public Iterator<Integer> iterator() {
+    return new Iter<Integer>(this.data, this.size);
+  }
+
+  public class Iter<T> implements Iterator<T> {
+
+    private final T[] data;
+    private final int length;
+    private int index = 0;
+
+    public Iter(T[] data, int length) {
+      this.data = data;
+      this.length = length;
+
+      if (data == null)
+        throw new IllegalArgumentException("data");
+
+      if (length > data.length || length < 0)
+        throw new IllegalArgumentException("length");
+    }
+
+    @Override
+    public boolean hasNext() {
+      return index < length;
+    }
+
+    @Override
+    public T next() {
+      if (!hasNext())
+        throw new NoSuchElementException();
+
+      return data[index++];
+    }
+
+    @Override
+    public void remove() {
+      throw new UnsupportedOperationException();
+    }
+
+  }
+
+  // ArrayList methods
+  //
+  // .addAll(Collection)
+  // .toArray()
 }
--- Iterators/src/main/java/collections/ArrayListTests.java
+++ Iterators/src/main/java/collections/ArrayListTests.java
@@ -1,17 +1,172 @@
 package collections;
 
 import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
 
+import java.util.Iterator;
+
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ArrayListTests {
 
   @Test
   public void initialSizeOfArrayShouldBeZero() {
-
     ArrayList list = new ArrayList();
     assertEquals(0, list.size());
+  }
+
+  @Test
+  public void initialSizeOfArrayShouldBeZeroEvenWithInitialCapacityOf20() {
+    ArrayList list = new ArrayList(20);
+    assertEquals(0, list.size());
+  }
+
+  @Test
+  public void shouldAddElement() {
+    ArrayList list = new ArrayList();
+    boolean res = list.add(42);
+    assertTrue(res);
+    assertEquals(1, list.size());
+  }
+
+  @Test
+  public void shouldAddTreeElement() {
+    ArrayList list = new ArrayList();
+    assertTrue(list.add(42));
+    assertTrue(list.add(2));
+    assertTrue(list.add(10));
+    assertEquals(3, list.size());
+  }
+
+  @Test
+  public void shouldGetValueWithIndex() {
+    ArrayList list = new ArrayList();
+    assertTrue(list.add(42));
+    assertTrue(list.add(2));
+    assertTrue(list.add(10));
+
+    Integer valueAt0 = list.get(0);
+    assertEquals(42, valueAt0.intValue());
+
+    Integer valueAt1 = list.get(1);
+    assertEquals(2, valueAt1.intValue());
+
+    Integer valueAt2 = list.get(2);
+    assertEquals(Integer.valueOf(10), valueAt2);
+  }
+
+  @Test
+  @Ignore
+  public void shouldGrowOfCapacityWasReached() {
+    ArrayList list = new ArrayList(2);
+    assertTrue(list.add(42));
+    assertTrue(list.add(2));
+
+    assertTrue(list.add(10));
+  }
+
+  @Test
+  public void shouldIterateOverArrayList() {
+    int[] expectedValues = { 42, 2, 10 };
+    int currentExpectedIndex = 0;
+
+    ArrayList list = new ArrayList();
+    for (int v : expectedValues)
+      list.add(v);
+
+    Iterator<Integer> iter = list.iterator();
+    while (iter.hasNext()) {
+      Integer val = iter.next();
+      System.out.println(val);
+      assertEquals(
+          expectedValues[currentExpectedIndex++],
+          val.intValue());
+    }
+
+    assertEquals(list.size(), currentExpectedIndex);
 
   }
+
+  @Test
+  public void shouldUseIndexOfToReturnIndexOfNullValueAndNonNullValue() {
+    ArrayList list = new ArrayList(10);
+    list.add(42);
+    list.add(2);
+    list.add(null);
+    list.add(10);
+    list.add(3);
+    list.add(5);
+
+    int idx = list.indexOf(null);
+    assertEquals(2, idx);
+
+    idx = list.indexOf(3);
+    assertEquals(4, idx);
+
+  }
+
+  @Test
+  public void shouldClearArrayWithElements() {
+    ArrayList list = new ArrayList(10);
+    list.add(42);
+    list.add(2);
+    list.add(null);
+    list.add(10);
+    list.add(3);
+    list.add(5);
+    assertEquals(6, list.size());
+
+    list.clear();
+    assertTrue(list.isEmpty());
+    assertEquals(0, list.size());
+  }
+
+  @Test
+  public void shouldUseIndexOfWithEqualsOfArgumentToFindElement() {
+    ArrayList list = new ArrayList(10);
+    list.add(42);
+    list.add(2);
+    list.add(null);
+    list.add(10);
+    list.add(3);
+    list.add(5);
+
+    int idx = list.indexOf(new FindByText("10"));
+    assertEquals(3, idx);
+
+    idx = list.indexOf(new FindByText("1234"));
+    assertEquals(-1, idx);
+
+    assertTrue(list.contains(new FindByText("2")));
+  }
+
+  private static class FindByText {
+    private final String text;
+
+    public FindByText(String text) {
+      this.text = text;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      System.out.println("DEBUG: I'm executing equals against " + obj);
+      if (obj == null)
+        return false;
+      return text.equals(obj.toString());
+    }
+
+  }
+
+  // ArrayList methods
+  //
+  // .get(idx) : E
+  // .indexOf(obj) : int
+  // .addAll(Collection)
+  // .clear()
+  // .contains(obj) : boolean
+  // .isEmpty()
+  // .iterator(): Iterator
+  // .toArray()
 
 }
